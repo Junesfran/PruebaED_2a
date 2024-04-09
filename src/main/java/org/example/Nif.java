408d8059a25c14c96a1508dd627556adf00d51fa
unknown <Diurno@C08PC14.informatica.comercio>
2024-04-09 10:23:58 +0200

    Documentada clase Nif

--- src/main/java/org/example/Nif.java
+++ src/main/java/org/example/Nif.java
@@ -6,7 +6,8 @@ package org.example;
 
 /**
  *
- * @author ProfDiurno
+ * @author Néstor Franco
+ * ver. 1.0.0.0.0.0.0.0.0.0.12
  */
 public class Nif {
 
@@ -14,35 +15,63 @@ public class Nif {
     private char letra;
 
     //CONSTANTE: no accesible - compartido por todo - no modificable
+
     private static final char[] LETRAS
             = {'T', 'R', 'W', 'A', 'G', 'M', 'Y', 'F', 'P', 'D',
                 'X', 'B', 'N', 'J', 'Z', 'S', 'Q', 'V', 'H', 'L',
                 'C', 'K', 'E'};
 
+    //Testear
     private static char calcularLetra(int numero) {
         return LETRAS[numero % 23];
     }
 
+    /**
+     *
+     *Constructor por defecto de la clase NIF, dejara el número del DNI a 0 y [espacio vacío] como letra
+     */
     protected Nif() {
         this.numero = 0;
         this.letra = ' ';
     }
 
+    /**
+     *
+     *Constructor de la clase NIF, recibe el número del NIF y asignará automáticamente  la letra.
+     * @param numero
+     */
     protected Nif(int numero) {
         this.numero = numero;
         this.letra = calcularLetra(numero);
     }
 
+    /**
+     *
+     *toString de la clase NIF devolverá de manera clara el NIF con el númnero y la letra asignados ya unidos.
+     * @return NIF
+     */
     @Override
     public String toString() {
         return numero + "-" + letra;
     }
 
+    /**
+     *
+     *Asignación de un nuevo número de NIF en caso de necesitar modificarlo o no tener uno asigando previamente. asignara
+     * una nueva letra al número.
+     * @param numero
+     */
     protected void setNif(int numero) {
         this.numero = numero;
         this.letra = calcularLetra(numero);
     }
 
+    /**
+     *
+     *Comprobará que dos objetos del tipo NIF sean o no iguales para evitar repeticiones.
+     * @param obj
+     * @return boolean
+     */
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
