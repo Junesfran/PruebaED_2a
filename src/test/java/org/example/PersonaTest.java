6847f332a86c28fb026c1ccf60250b952c12eb7f
unknown <Diurno@C08PC14.informatica.comercio>
2024-04-09 09:00:52 +0200

    Implementación test PersonaTest

---
+++ src/test/java/org/example/PersonaTest.java
@@ -0,0 +1,46 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PersonaTest {
+    Persona p;
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+        p = new Persona();
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getEdad() {
+        LocalDate pruebas;
+
+        //El dato por defecto es un buen dato
+        assertEquals(34, p.getEdad(), "Valor bueno");
+
+        //Valor malo
+        try {
+            pruebas = LocalDate.of(1,1,2025);
+            p.setNacimiento(pruebas);
+            assertEquals(-1, p.getEdad(), "Error por una edad con la que no ha nacido");
+        }catch (Exception e){
+            System.out.println("El código ha dado error y no -1");
+        }
+
+        //Valor null
+        try {
+            pruebas = null;
+            p.setNacimiento(pruebas);
+            assertEquals(0, p.getEdad(), "Error por null");
+        }catch (Exception e){
+            System.out.println("El código ha dado error y no ha devueleto null");
+        }
+    }
+}
