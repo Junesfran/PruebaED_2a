9572543d9652dd79fdabd05dca8a6088ccfb921e
unknown <Diurno@C08PC14.informatica.comercio>
2024-04-09 09:49:08 +0200

    Implementación test CursoTest.java

--- src/test/java/org/example/CursoTest.java
+++ src/test/java/org/example/CursoTest.java
@@ -1,4 +1,35 @@
+package org.example;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class CursoTest {
-  
+
+    Curso a;
+    @BeforeEach
+    void setUp() {
+        a = new Curso("DAM");
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void aniadirAlumno() {
+        Persona alumno1 = new Persona();
+        //Caso bueno
+        a.aniadirAlumno(alumno1);
+
+
+        //Caso null
+        try {
+            a.aniadirAlumno(null);
+        }catch (Exception e){
+            fail("Ha saltado un error y no un null");
+        }
+    }
 }
