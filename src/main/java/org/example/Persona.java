6405b8b0fea638c2ff3c25b6f200ec994a5ac876
unknown <Diurno@C08PC14.informatica.comercio>
2024-04-09 10:24:12 +0200

    Documentada clase Persona

--- src/main/java/org/example/Persona.java
+++ src/main/java/org/example/Persona.java
@@ -15,6 +15,10 @@ public class Persona implements Comparable<Persona> {
     private char genero;
     private LocalDate nacimiento;
 
+    /**
+     *
+     *Constructor por defecto de la clase persona asignara que la fecha de nacimiento por defecto es 1/1/1990.
+     */
     public Persona() {
         nif = new Nif();
         nombre = "";
@@ -22,11 +26,26 @@ public class Persona implements Comparable<Persona> {
         nacimiento = LocalDate.of(1990, 1, 1);
     }
 
+    /**
+     *
+     *Constructor de la clase Persona, recibira unicamente un número NIF
+     * @param nif
+     */
     public Persona(int nif) {
         this();
         this.nif = new Nif(nif);
     }
 
+    /**
+     *
+     *Constructor de la clase Persona, recibira un número de NIF el nombre del alumno, su genero y la fecha de su nacimiento
+     * @param nombre
+     * @param nif
+     * @param genero
+     * @param dia
+     * @param mes
+     * @param ano
+     */
     public Persona(int nif, String nombre, char genero,
             int dia, int mes, int ano) {
         this.nif = new Nif(nif);
@@ -68,10 +87,21 @@ public class Persona implements Comparable<Persona> {
         this.nacimiento = nacimiento;
     }
 
+    //testear
+    /**
+     *
+     *Método que devolvera la edad actual del alumno.
+     * @return int edad.
+     */
     public int getEdad() {
         return Period.between(nacimiento, LocalDate.now()).getYears();
     }
 
+    /**
+     *
+     *toString de la clase Persona devolvera de manera clara y ordenada los datos de la persona
+     * @return String con datos del alumno
+     */
     @Override
     public String toString() {
         if (nombre.split(" ").length > 1) {
@@ -82,6 +112,12 @@ public class Persona implements Comparable<Persona> {
         }
     }
 
+    /**
+     *
+     *Método que comprueba si los NIF de los alumnos son iguales. Recibé un objeto del tipo Persona
+     * @param a
+     * @return boolean
+     */
     public boolean equals(Persona a) {
         if (a == null) {
             return false;
@@ -89,7 +125,6 @@ public class Persona implements Comparable<Persona> {
         return a.nif.toString().equals(this.nif.toString());
     }
 
-    @Override
     public boolean equals(Object obj) {
         if (this == obj) {
             return true;
@@ -104,7 +139,10 @@ public class Persona implements Comparable<Persona> {
 
         return Objects.equals(this.nif, other.nif);
     }
-
+    /**
+     *
+     *CompareTo de la clase Persona que ordenara ayudará a ordenar en el listado del curso a los alimnos por NIF
+     */
     @Override
     public int compareTo(Persona o) {
         return this.nif.toString().compareTo(o.nif.toString());
